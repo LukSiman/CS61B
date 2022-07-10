e87f60f9f3f7467619c0d8039012f59730df1981
LukSiman <simanavicius.lukas@gmail.com>
2022-07-10 12:03:55 -0700

    implemented addAdjacent method

--- lectureCode-sp21/lec5_lists2/IntList.java
+++ lectureCode-sp21/lec5_lists2/IntList.java
@@ -1,44 +1,65 @@
 public class IntList {
-	public int first;
-	public IntList rest;
+    public int first;
+    public IntList rest;
 
-	public IntList(int f, IntList r) {
-		first = f;
-		rest = r;
-	}
+    public IntList(int f, IntList r) {
+        first = f;
+        rest = r;
+    }
 
-	/** Return the size of the list using... recursion! */
-	public int size() {
-		if (rest == null) {
-			return 1;
-		}
-		return 1 + this.rest.size();
-	}
+    /**
+     * Return the size of the list using... recursion!
+     */
+    public int size() {
+        if (rest == null) {
+            return 1;
+        }
+        return 1 + this.rest.size();
+    }
 
-	/** Return the size of the list using no recursion! */
-	public int iterativeSize() {
-		IntList p = this;
-		int totalSize = 0;
-		while (p != null) {
-			totalSize += 1;
-			p = p.rest;
-		}
-		return totalSize;
-	}
+    /**
+     * Return the size of the list using no recursion!
+     */
+    public int iterativeSize() {
+        IntList p = this;
+        int totalSize = 0;
+        while (p != null) {
+            totalSize += 1;
+            p = p.rest;
+        }
+        return totalSize;
+    }
 
-	/** Returns the ith item of this IntList. */
-	public int get(int i) {
-		if (i == 0) {
-			return first;
-		}
-		return rest.get(i - 1);
-	}
+    /**
+     * Returns the ith item of this IntList.
+     */
+    public int get(int i) {
+        if (i == 0) {
+            return first;
+        }
+        return rest.get(i - 1);
+    }
 
-	public static void main(String[] args) {
-		IntList L = new IntList(15, null);
-		L = new IntList(10, L);
-		L = new IntList(5, L);
+    public void addAdjacent() {
+        IntList p = this;
 
-		System.out.println(L.get(100));
-	}
+        while (p != null && p.rest != null) {
+            if (p.first == p.rest.first) {
+                p.first *= 2;
+                p.rest = p.rest.rest;
+            }
+            if(p.first != p.rest.first){
+                p = p.rest;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        IntList L = new IntList(3, null);
+        L = new IntList(2, L);
+        L = new IntList(1, L);
+        L = new IntList(1, L);
+        L.addAdjacent();
+        System.out.println(L.size());
+    }
 } 
