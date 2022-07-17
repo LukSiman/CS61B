f5597a0c5054a7f6583099f964bcf81e83801326
LukSiman <simanavicius.lukas@gmail.com>
2022-07-17 12:16:06 -0700

    implemented the Alist class

--- lectureCode-sp21/exercises/lists4/AList.java
+++ lectureCode-sp21/exercises/lists4/AList.java
@@ -3,31 +3,54 @@
  */
 
 public class AList {
+    private int[] items;
+    private int size;
+
     /** Creates an empty list. */
     public AList() {
+        items = new int[100];
+        size = 0;
     }
 
     /** Inserts X into the back of the list. */
     public void addLast(int x) {
+        if(size == items.length){
+            increaseSizeOfArray();
+        }
+        items[size] = x;
+        size++;
+    }
+
+    private void increaseSizeOfArray(){
+        int[] biggerItems = new int[items.length*2];
+
+        for(int i = 0; i<items.length; i++){
+            biggerItems[i] = items[i];
+        }
+
+        items = biggerItems;
     }
 
     /** Returns the item from the back of the list. */
     public int getLast() {
-        return 0;        
+        return items[size-1];
     }
     /** Gets the ith item in the list (0 is the front). */
     public int get(int i) {
-        return 0;        
+        return items[i];
     }
 
     /** Returns the number of items in the list. */
     public int size() {
-        return 0;        
+        return size;
     }
 
     /** Deletes item from back of the list and
       * returns deleted item. */
     public int removeLast() {
-        return 0;
+        int deletedItem = getLast();
+        size--;
+
+        return deletedItem;
     }
 } 
